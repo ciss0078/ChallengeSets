c79e75c9b20d00968390f87680897a68c4842407
Abdoulahi Cisse <sentel.nv@gmail.com>
2020-10-30 15:54:58 -0700

    solved challenge set 1

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -5,32 +5,59 @@ namespace ChallengeSets
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if(num1 == num2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            double diff = minuend - subtrahend;
+            return diff;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            int sum = number1 + number2;
+            return sum;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return Math.Min(number1, number2);
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            long product = factor1 * factor2;
+            return product;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if(nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else
+            {
+                return  $"Hello, {nameOfPerson}!";
+            }
+             
+           
+           
+            
         }
 
         public string GetHey()
