662428805cf44f375f3db72a89910c2bdb5c9ca7
Abdoulahi Cisse <sentel.nv@gmail.com>
2020-12-17 14:28:03 -0800

    solved challenge set 6

--- ChallengeSets/ChallengeSet06.cs
+++ ChallengeSets/ChallengeSet06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengeSets
 {
@@ -7,27 +8,160 @@ namespace ChallengeSets
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            
+            
+            //throw new NotImplementedException();
+            if (words == null || words.Contains(null))
+            {
+                return false;
+            }
+            
+           
+
+            var newL = words.Select(x => x.ToString().ToLower());
+
+
+            if (ignoreCase == false)
+            {
+                if (words.Contains(word) == false)
+                {
+                    return false;
+                }
+                else 
+                {
+                    return true;
+                } 
+                    
+                
+
+            }
+            else 
+            {
+                if (!newL.Contains(word.ToLower()) )
+                {
+                    return false;
+                }
+               
+
+                return true;
+                
+            }
+            
+            
+
+            
+
+
+
+           
+
+
+
+
+
+
+
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            if (num <= 1)
+            {
+                return false;
+            }
+            if (num % 2 == 0 && num != 2)
+            {
+                return false;
+            }
+            if (num % 3 == 0 && num != 3)
+            {
+                return false;
+            }
+
+            if (num % num != 0 && num / 1 != num && num / num != 1)
+            {
+                return false;
+            }
+
+            return true;
         }
+
 
         public int IndexOfLastUniqueLetter(string str)
-        {
-            throw new NotImplementedException();
-        }
+        { int index = -1;
+            bool conditional;
+            //throw new NotImplementedException();
+            for(int i = 0; i < str.Length; i++)
+            {
+                conditional = true;
+                
+                for(int j= 0; j < str.Length; j++)
+                {
+                    if(str[i] == str[j] && i != j) 
+                    {
+                        conditional = false;
+                    }
+                    
+                }
 
+                if(conditional == true)
+                {
+                    index = i;
+                }
+                
+
+            }
+
+            return index;
+        }
         public int MaxConsecutiveCount(int[] numbers)
-        {
-            throw new NotImplementedException();
+        { 
+            int maxCount = 0;
+            //throw new NotImplementedException();
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                int counter = 1;
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    if(numbers[i] != numbers[j])
+                    {
+                        break;
+                    }
+                    counter++;
+                }
+                if(counter > maxCount)
+                {
+                    maxCount = counter;
+                }
+            }
+            return maxCount;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            double[] check = new double[0] ;
+            if(elements == null)
+            {
+                return check;
+            }
+            var newEveryNEl = new List<double>();
+            for(int i = 0; i < elements.Count; i++)
+            {
+               
+                if(elements[i] % n == 0)
+                {
+                    newEveryNEl.Add(elements[i]);
+                }
+                if(n < 0 || n > elements.Count)
+                {
+                    newEveryNEl.Clear();
+                }
+                    
+                
+            }
+            return newEveryNEl.ToArray();
         }
     }
 }
